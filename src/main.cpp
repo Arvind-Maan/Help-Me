26781eb06161758636a10bd7c76f32d8b02225a9
Arvind Maan <arvindmaan1199@gmail.com>
2020-06-20 01:16:09 -0500

    set up wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,44 @@
 #include <Arduino.h>
-/*
-  Blink
+#include <ESP8266WiFi.h>
+#include <WifiClientSecure.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+//****TO DO: Fill out the following variables with your information******
+const char *ssid = "";     // your SSID
+const char *password = ""; //your wifi password
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+//your secret key from http://maker.ifttt.com.
+//(can be found in settings or https://ifttt.com/maker_webhooks/settings)
+const char *makerKey = "";
+//******************** end of TO-DO ********************
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
+const char *host = "maker.ifttt.com";
+const int port = 443;
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-  // initialize digital pin LED_BUILTIN as an output.
+  //initialize LED
   pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+
+  //connect to wifi
+  Serial.println("connecting to:");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    //flash since we haven't connected to wifi yet
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(250);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    delay(250);                      // wait for a second
+    Serial.print(".");
+  }
+  Serial.println("\nWifi connected!\nIP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
 }
